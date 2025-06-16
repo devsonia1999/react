bcf6ac9f9eedeade80c816599c57dbb1a2ed2f20
devsonia1999 <devsonia.js@gmail.com>
2025-06-16 18:00:38 +0900

    set

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
